5199460329b8fbd0ca1cc20b6c45b9e06bd1e76b
YongWan Jo <yongwan.jo@aveva.com>
2012-11-13 11:30:50 +0900

    Add MyFirstClass

---
+++ MyFirstApplication/MyFirstClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyFirstApplication
+{
+    public class MyFirstClass
+    {
+        public static string MyFirstMethod()
+        {
+            return "This is my first method!";
+        }
+    }
+}
